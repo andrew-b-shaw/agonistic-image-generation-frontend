77aa0f94294fd2df004d00cfd2488b19811c4579
Andrew Shaw <shawan@uw.edu>
2024-09-13 21:38:33 -0700

    Update build path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    basePath: "/agonistic-image-generation-frontend"
 };
 
 export default nextConfig;
